2c850fa24b5383bd3c4c9446f292439dbf266255
Salome Karseladze <saly13071@gmail.com>
2025-03-21 23:46:50 +0400

    fixed  employee input with

--- src/components/comments/task/Task.jsx
+++ src/components/comments/task/Task.jsx
@@ -89,51 +89,5 @@ export default function Task() {
           ))
         : "Loading..."}
     </div>
-    // <div className={styles.tasksMainContainer}>
-    //   <div className={styles.tasksStatusContainer}>
-    //     <div className={styles.taskItemName}>
-    //       <p>დასაწყები</p>
-    //     </div>
-    //     <div>
-    //       <TaskItem />
-    //       <TaskItem />
-    //       <TaskItem />
-    //       <TaskItem />
-    //     </div>
-    //   </div>
-    //   <div className={styles.tasksStatusContainer}>
-    //     <div className={styles.taskItemName2}>
-    //       <p>პროგრესში</p>
-    //     </div>
-    //     <div>
-    //       <TaskItem className={styles.taskItemName2color} />
-    //       <TaskItem />
-    //       <TaskItem />
-    //       <TaskItem />
-    //     </div>
-    //   </div>
-    //   <div className={styles.tasksStatusContainer}>
-    //     <div className={styles.taskItemName3}>
-    //       <p>მზად ტესტირებისთვის</p>
-    //     </div>
-    //     <div>
-    //       <TaskItem />
-    //       <TaskItem />
-    //       <TaskItem />
-    //       <TaskItem />
-    //     </div>
-    //   </div>
-    //   <div className={styles.tasksStatusContainer}>
-    //     <div className={styles.taskItemName4}>
-    //       <p>დასრულებული</p>
-    //     </div>
-    //     <div>
-    //       <TaskItem />
-    //       <TaskItem />
-    //       <TaskItem />
-    //       <TaskItem />
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
